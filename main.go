8dfbc8c7a2d905919d7d4186855c23522a5530a2
Daniel Lopez <d.lopez.1740@gmail.com>
2025-08-17 17:35:19 -0400

    Add User and Task models with user routes
    Introduces User and Task models using GORM, sets up database migrations, and implements basic user-related HTTP handlers (GET, POST, DELETE) in the users routes. Updates main.go to register the new routes and perform model migrations at startup.

--- main.go
+++ main.go
@@ -4,15 +4,25 @@ import (
 	"net/http"
 
 	"github.com/Daniel1024/go-gorm-restapi/db"
+	"github.com/Daniel1024/go-gorm-restapi/models"
 	"github.com/Daniel1024/go-gorm-restapi/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	db.DBConnection()
+
+	db.DB.AutoMigrate(models.Task{})
+	db.DB.AutoMigrate(models.User{})
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", routes.HomeHandler)
+
+	r.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
+	r.HandleFunc("/users/{id}", routes.GetUserHandler).Methods("GET")
+	r.HandleFunc("/users", routes.PostUserHandler).Methods("POST")
+	r.HandleFunc("/users/{id}", routes.DeleteUserHandler).Methods("DELETE")
 
 	http.ListenAndServe(":3000", r)
 }
--- routes/users.routes.go
+++ routes/users.routes.go
@@ -1,1 +1,39 @@
 package routes
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Daniel1024/go-gorm-restapi/db"
+	"github.com/Daniel1024/go-gorm-restapi/models"
+)
+
+func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
+	var users []models.User
+	db.DB.Find(&users)
+	json.NewEncoder(w).Encode(&users)
+}
+
+func GetUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("GetUserHandler"))
+}
+
+func PostUserHandler(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+
+	json.NewDecoder(r.Body).Decode(&user)
+
+	createUser := db.DB.Create(&user)
+	err := createUser.Error
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	json.NewEncoder(w).Encode(&createUser)
+}
+
+func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("DeleteUserHandler"))
+}
