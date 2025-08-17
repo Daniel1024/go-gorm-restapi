641401abae4a1829d9cc605574090cda222b2ec6
Daniel Lopez <d.lopez.1740@gmail.com>
2025-08-17 15:08:32 -0400

    Add PostgreSQL integration with GORM and Docker Compose
    Introduced a new db/connection.go file to handle PostgreSQL connections using GORM. Added a docker-compose.yaml for local PostgreSQL and Adminer setup. Updated go.mod and go.sum to include GORM and PostgreSQL driver dependencies. main.go now initializes the database connection at startup.

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"github.com/Daniel1024/go-gorm-restapi/db"
 	"github.com/Daniel1024/go-gorm-restapi/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	db.DBConnection()
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", routes.HomeHandler)
